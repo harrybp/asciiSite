e8ad16315be19b1aa1c0c9ee29f0735b75ed5a75
Harry <harrytbp@gmail.com>
2018-03-21 21:51:56 +0800

    Perfected navbar method, added comments

--- ascii.js
+++ ascii.js
@@ -1,128 +1,144 @@
-function getCharacterWidth(){
-	var sizingSpan = document.createElement("span");
-	sizingSpan.innerHTML = '--------------------';
-	sizingSpan.style.cssText += 'position: absolute; top: -100px; padding: 0px;';
-	document.body.insertBefore(sizingSpan, document.body.firstChild);
-	sizingSpan.parentNode.removeChild(sizingSpan);
-	return sizingSpan.clientWidth/20;
-}
+//-----------------------------------------------------------------------------
+//  ASCII Webpage
+//    - By HarryBP
+//
+//  Define properties with config
+      var config = { width: 700 }
+//
+//  Define page structure with build() method
+      function build(){
+        drawLine('-');
+        writeCentered('Harry Bond-Preston');
+        drawLine('-');
+        var links = [
+          { text: 'About Me', link: '#', selected: true },
+          { text: 'Web Projects', link: '#', selected: false },
+          { text: 'Github', link: '#', selected: false }
+        ]
+        navbar(links);
+        drawLine('-');
+        writeCentered('');
+        writeCentered('About Me');
+        writeCentered('');
+        drawLine('-');
+      }
+//
+//-----------------------------------------------------------------------------
 
-function drawLine() {
+
+
+
+//-----------------------------------------------------------------------------
+//               _____ _____ 
+//         /\   |  __ \_   _|     drawLine()
+//        /  \  | |__) || |       writeCentered(text)
+//       / /\ \ |  ___/ | |       navbar(links)
+//      / ____ \| |    _| |_      
+//     /_/    \_\_|   |_____|
+//
+//-----------------------------------------------------------------------------
+//  Draws a line of given character across the screen width
+function drawLine(char) {
   var finished = getGlobalLeftPadding();;
-	for(var x = 0; x < pageWidth-1; x++) finished += '-';
-  return finished + '\n';
+  for(var x = 0; x < pageWidth; x++) finished += char;
+  text +=  (finished + '\n');
 }
 
-function writeCentered(text){
+//-----------------------------------------------------------------------------
+//  Writes given text centered in the screen
+function writeCentered(content){
   var finished = getGlobalLeftPadding() + '|';
-  var leftPadding = Math.floor((pageWidth-4 - text.length) /2);
-  var rightPadding = pageWidth-4 - leftPadding - text.length ;
+  var leftPadding = Math.floor((pageWidth - 2 - content.length) /2);
+  var rightPadding = pageWidth - 2 - leftPadding - content.length ;
   for(x = 0; x < leftPadding; x++) finished += '&nbsp;';
-  finished += text;
+  finished += content;
   for(x = 0; x < rightPadding; x++) finished += '&nbsp;';
-  return finished + ' | \n';
+  text += (finished + '|\n');
 }
 
+//-----------------------------------------------------------------------------
+//  Adds a navbar given an array of links objects:
+//    { 
+//      text:     (String - The link display text),
+//      link:     (String - The link url),
+//      selected: (Boolean - True if link is for current page)
+//    }
 function navbar(links){
-  var finished = getGlobalLeftPadding()+'|';
-  var widthPerNav = Math.floor((pageWidth - 2 - links.length-1) / links.length);
-  var shortage = pageWidth - (widthPerNav*3 + 2 + 3);
+  var widthPerNav = Math.floor(((pageWidth - 2 - (links.length-1)) / links.length));
+  var missed = pageWidth - (links.length*widthPerNav) - links.length-1;
   var largest = 0;
   for(var linkIndex in links){
     var link = links[linkIndex];
-    if(link.text.length + 2 > largest) largest = link.text.length + 2;
+    if(link.text.length > largest) largest = link.text.length;
   }
-  var totalLength = (largest * links.length) + links.length - 1 + 8;
-  if(totalLength > pageWidth){
-    var finished = '';
-    for(var linkIndex in links){
-      var link = links[linkIndex];
-      finished += navbar([link]);
+
+  //Catch small screens
+  if(largest+2 > widthPerNav){
+    if(links.length > 1){
+      var total = '';
+      for(var linkI in links){
+        navbar([links[linkI]]);
+      }
+      return;
     }
-    return finished;
   }
 
-
+  var finished = getGlobalLeftPadding()+'|';
   for(var linkIndex in links){
     var link = links[linkIndex];
     var leftPadding = Math.floor((widthPerNav - 2 - link.text.length) /2);
     var rightPadding = widthPerNav - 2 - leftPadding - link.text.length;
     for(x = 0; x < leftPadding; x++) {
-      if(link.selected) finished += '#';
+      if(link.selected) finished += '>';
       else finished += '&nbsp;';
     }
     finished += '[<a href=\'' + link.link + '\'>' + link.text + '</a>]';
     for(x = 0; x < rightPadding; x++) {
-      if(link.selected) finished += '#';
+      if(link.selected) finished += '<';
       else finished += '&nbsp;';
     }
-    if(shortage > 0){
-      if(link.selected) finished += '#';
+    if(missed > 0){
+      if(link.selected) finished += '<';
       else finished += '&nbsp;';
-      shortage--;
+      missed--;
     }
-    if(linkIndex < links.length-1) finished += '|';  
+    if(linkIndex < links.length-1) finished += '|';
   }
-  return finished + '|\n';
+  text += finished + '|\n';
 }
 
-function writeMultiCentered(text){
-  var finished = '';
-  var lines = text.split('\n');
-  for(var lineIndex in lines)
-    finished+=writeCentered(lines[lineIndex]);
-  return finished;
-
+//-----------------------------------------------------------------------------
+//  Helper functions
+function getCharacterWidth(){
+	var sizingSpan = document.createElement("span");
+	sizingSpan.innerHTML = '--------------------';
+	sizingSpan.style.cssText += 'position: absolute; top: -100px; padding: 0px;';
+	document.body.insertBefore(sizingSpan, document.body.firstChild);
+  var width = sizingSpan.clientWidth/20;
+	sizingSpan.parentNode.removeChild(sizingSpan);
+	return width;
 }
 function getGlobalLeftPadding(){
   var text = '';
   for(var x = 0; x < globalLeftPadding; x++) text += '&nbsp;';
   return text;
 }
-function addGlobalRightPadding(text){
-  for(var x = 0; x < globalRightPadding; x++) text += '&nbsp;';
-  return text;
-}
-
-function go(){
+function calculate(){
+  var configuration = (typeof config !== 'undefined')? config : {width:700};
+  var siteWidth = (configuration.width);
+  text = '';
   document.body.innerHTML = '';
-	var charWidth = getCharacterWidth();
-	pageWidth = Math.floor(window.innerWidth/9.6);
-  intWidth = Math.floor(700/9.6);
+  var charWidth = getCharacterWidth();
+  pageWidth = Math.floor(window.innerWidth/charWidth);
+  intWidth = Math.floor(siteWidth/charWidth);
   if(intWidth < pageWidth){
-    var difference = pageWidth-intWidth;
-    globalLeftPadding = Math.floor(difference/2);
-    globalRightPadding = difference - globalLeftPadding;
+    globalLeftPadding = Math.floor((pageWidth-intWidth)/2);
     pageWidth = intWidth;
   } else {
-    globalLeftPadding = globalRightPadding = 0;
+    globalLeftPadding = 0;
   }
-
-
-  var text = '';
-	text += drawLine();
-  text += writeCentered('Harry Bond-Preston')
-  text += drawLine();
-
-  var links = [
-    { text: 'About Me', link: '#', selected: true },
-    { text: 'Web Projects', link: '#', selected: false },
-    { text: 'Github', link: '#', selected: false }
-  ]
-  text += navbar(links);
-  text += drawLine();
-  text += writeCentered('');
-  text += writeCentered('About Me');
-  text += writeCentered('');
-  text += drawLine();
-
-
+  build();
   document.body.innerHTML = text;
-
 }
-
-go();
-
-window.onresize = function(event) {
-  go();
-};
+window.onload = function(){ calculate() };
+window.onresize = function(event) { calculate(); };
